6b73b36df91a0edd83a3d582e28c5e87608a883b
mommark <123541430+mommark@users.noreply.github.com>
2020-02-11 12:57:38 +0000

    daf

--- AudioPlayer.cpp
+++ AudioPlayer.cpp
@@ -10,4 +10,5 @@
 
 static const int kNumberBuffers = 3;                              // 1
 struct AQPlayerState {
-    AudioStreamBasicDescription   mDataFormat;                    
+    AudioStreamBasicDescription   mDataFormat;                    // 2
+    AudioQ
