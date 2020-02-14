af14a897f2be8eab97c42cd3b8951c555a4926f7
mommark <123541430+mommark@users.noreply.github.com>
2020-02-14 21:20:27 +0000

    Delete .pydevproject

--- AudioPlayer.cpp
+++ AudioPlayer.cpp
@@ -12,4 +12,4 @@ static const int kNumberBuffers = 3;                              // 1
 struct AQPlayerState {
     AudioStreamBasicDescription   mDataFormat;                    // 2
     AudioQueueRef                 mQueue;                         // 3
-    Audio
+    AudioQueueBufferRe
