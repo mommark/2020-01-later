ae669dda7d69ece97fe63880ff4be68460fffacc
mommark <123541430+mommark@users.noreply.github.com>
2020-03-26 08:02:27 +0000

    build(deps): bump path-parse from 1.0.6 to 1.0.7

--- AudioPlayer.cpp
+++ AudioPlayer.cpp
@@ -18,4 +18,10 @@ struct AQPlayerState {
     SInt64                        mCurrentPacket;                 // 7
     UInt32                        mNumPacketsToRead;              // 8
     AudioStreamPacketDescription  *mPacketDescs;                  // 9
-    bool                          mIsRunning;                   
+    bool                          mIsRunning;                     // 10
+};
+
+
+
+
+static void
