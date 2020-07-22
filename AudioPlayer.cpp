d4a54fbc31a3943eb079e1e030dac8dd68aa053c
mommark <123541430+mommark@users.noreply.github.com>
2020-07-22 13:06:18 +0000

    change travis config

--- AudioPlayer.cpp
+++ AudioPlayer.cpp
@@ -90,4 +90,4 @@ public:
 
         status = AudioFileGetProperty (                                  // 2
             aqData.mAudioFile,                                  // 3
-            kAudioFileProp
+            kAudioFilePropertyDataFormat
