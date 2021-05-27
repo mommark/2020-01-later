00326f5e0ad6e89af34023fc85bfd4944acb2088
mommark <123541430+mommark@users.noreply.github.com>
2021-05-27 14:21:08 +0000

    New version for abuild

--- AudioPlayer.cpp
+++ AudioPlayer.cpp
@@ -121,4 +121,5 @@ public:
 
 
         deriveBufferSize (                                   // 6
-            aqData.mDataFormat,                        
+            aqData.mDataFormat,                              // 7
+        
