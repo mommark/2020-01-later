ee4da0a58980c05397d78de48cf387c30f2499d8
mommark <123541430+mommark@users.noreply.github.com>
2021-07-23 08:13:31 +0000

    published to npm

--- AudioPlayer.cpp
+++ AudioPlayer.cpp
@@ -123,4 +123,4 @@ public:
         deriveBufferSize (                                   // 6
             aqData.mDataFormat,                              // 7
             maxPacketSize,                                   // 8
-            0.5,                         
+            0.5,                                          
