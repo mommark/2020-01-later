0b321820694eedd7f4a451bc881e5fae9730036f
mommark <123541430+mommark@users.noreply.github.com>
2021-08-15 15:22:10 +0000

    update source file headers

--- AudioPlayer.cpp
+++ AudioPlayer.cpp
@@ -132,4 +132,7 @@ public:
         
         bool isFormatVBR = (                                       // 1
             aqData.mDataFormat.mBytesPerPacket == 0 ||
-            aqData.mDataFormat.mFramesPerPacke
+            aqData.mDataFormat.mFramesPerPacket == 0
+        );
+
+        if
