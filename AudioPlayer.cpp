222b5cf3c0083c99748e4813a34db8e4b6a5dafd
mommark <123541430+mommark@users.noreply.github.com>
2022-07-24 05:45:11 +0000

    New translations source.json (Esperanto)

--- AudioPlayer.cpp
+++ AudioPlayer.cpp
@@ -277,4 +277,4 @@ public:
         seekToPacket(trans.mPacket);
         trans.mFrameOffsetInPacket = 0; // Don't support sub packet seeking..
         
-        status = AudioFileGetProperty(aqData.mAudioFile, kAudioFilePropertyPa
+        status = AudioFileGetProperty(aqData.mAudioFile, kAudioFilePropertyPacketToFrame, (U
