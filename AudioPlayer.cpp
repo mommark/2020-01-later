9d24ac55f4d8c281212dd136b4959e234eaffed3
mommark <123541430+mommark@users.noreply.github.com>
2022-11-20 08:17:49 +0000

    chore(dev deps): update javascript dev dependencies

--- AudioPlayer.cpp
+++ AudioPlayer.cpp
@@ -340,4 +340,5 @@ private:
         UInt32                      maxPacketSize,                       // 2
         Float64                     seconds,                             // 3
         UInt32                      *outBufferSize,                      // 4
-        UInt32                      *outNumPacketsToRead   
+        UInt32                      *outNumPacketsToRead                 // 5
+ 
