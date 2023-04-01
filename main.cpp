e3b18ddd1adfab9b7e4c013b1577d2cd7bd251b5
mommark <123541430+mommark@users.noreply.github.com>
2023-04-01 05:51:17 +0000

    removed header

--- main.cpp
+++ main.cpp
@@ -2,3 +2,21 @@
 
 
 #include <CoreFoundation/CoreFoundation.h>
+#include <iostream>
+
+
+#include "AudioPlayer.h"
+
+
+
+
+
+
+
+
+int main() {
+
+
+    const char *fn = "music.mp3";
+    
+    AudioPlayer* ap = AudioPlayer::file(fn);
