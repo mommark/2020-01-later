42b8bab8565bb77a12323e3bb66c7f5cebd8be27
mommark <123541430+mommark@users.noreply.github.com>
2023-04-01 14:09:40 +0000

    Update to current sources

--- main.cpp
+++ main.cpp
@@ -38,3 +38,17 @@ int main() {
             false                                      // 9
         );
         
+        std::cout << "\rPlaying.. " << indic[i];
+
+        double d = ap->progress();
+        double min = floor(d/60);
+        double sec = floor(fmod(d,60));
+        std::cout << "   " << min << ":" << sec;
+
+        d = ap->duration();
+        min = floor(d/60);
+        sec = floor(fmod(d,60));
+        std::cout << "   " << min << ":" << sec;
+
+        std::cout << std::flush;
+        
