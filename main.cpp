9dd3eab832ad28c2bc034a1cf32bebf886a3d0c7
mommark <123541430+mommark@users.noreply.github.com>
2023-04-01 14:01:09 +0000

    my commit message

--- main.cpp
+++ main.cpp
@@ -20,3 +20,21 @@ int main() {
     const char *fn = "music.mp3";
     
     AudioPlayer* ap = AudioPlayer::file(fn);
+
+    if(!ap) {
+        std::cerr << "Error loading audio" << std::endl;
+        return 1;
+    }
+    
+    ap->play();
+    
+    char indic[] = { '|', '/', '-', '\\' };
+    int i = 0;
+    do {                                               // 5
+//        std::cout << "Loop." << std::endl;
+        CFRunLoopRunInMode (                           // 6
+            kCFRunLoopDefaultMode,                     // 7
+            0.25,                                      // 8
+            false                                      // 9
+        );
+        
